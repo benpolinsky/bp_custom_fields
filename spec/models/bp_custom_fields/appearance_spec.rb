6aa05a94987204fafa0a41689806ff9d2b4b625c
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-05-05 19:43:45 -0400

    cleanup

---
+++ spec/models/bp_custom_fields/appearance_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe BpCustomFields::Appearance, type: :model do
+  it "exists (pretty useless but hey with tdd it's a start)" do
+    expect(BpCustomFields::Appearance.new).to be_a BpCustomFields::Appearance
+  end
+  
+  it ""
+end
