de92ca30c8d6253724399d3bbf5e9e001fe81027
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-05-10 18:03:45 -0400

    datetime and date fields

--- app/assets/javascripts/bp_custom_fields/flatpickr_config.js
+++ app/assets/javascripts/bp_custom_fields/flatpickr_config.js
@@ -1,2 +1,3 @@
 $(document).ready(function() {
+  flatpickr('.bp-date-field');
 });
--- app/helpers/bp_custom_fields/fields_helper.rb
+++ app/helpers/bp_custom_fields/fields_helper.rb
@@ -1,8 +1,8 @@
 module BpCustomFields
   module FieldsHelper
-    def bpcf_html_options(field_template, object, extra_classes='')
+    def bpcf_html_options(field_template, object, options={})
       html_options = {}
-      html_options[:class] = "form-control #{extra_classes}"
+      html_options[:class] = "form-control #{options[:class]}" 
       html_options[:placeholder] = field_template.placeholder_text if field_template.placeholder_text.present? && object.value.blank?
       html_options[:value] = field_template.default_value if field_template.default_value.present? && object.value.blank?
       html_options[:required] = field_template.required
@@ -10,6 +10,7 @@ module BpCustomFields
       html_options[:maxlength] = field_template.max if field_template.max.present?
       html_options[:min] = field_template.min if field_template.min.present?
       html_options[:max] = field_template.max if field_template.max.present?
+      html_options[:data] = options[:data] if options[:data]
       html_options
     end
   end
