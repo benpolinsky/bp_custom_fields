84c16da4450df9e252c20b31d6a2fc9e386e8629
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-05-23 20:01:29 -0400

    begin theming

--- app/assets/javascripts/bp_custom_fields/themes/base-theme.js
+++ app/assets/javascripts/bp_custom_fields/themes/base-theme.js
@@ -1,13 +1,38 @@
 jQuery(document).ready(function($) {
+  // TODO: you need to gather each within a flex group... but one thing at a time
   // all add_links, replace with a plus, and move text to a hover state
-  $('.links .add_fields').each(function(index) {
+  
+  var fields = $('.links .add_fields')
+  var menu = '<div class="field-menu">+</div>';
+  $('body').append(menu);
+  
+  fields.each(function(index) {
+    current_template = $('.field-menu').data('template');
+    if (current_template == undefined) {current_template = ""}
+    $('.field-menu').data('template', (current_template + append_drop(this)));
     add_base_link_styles(this);
   });
-  $('.links').on('mouseover', 'a.add_fields', function(event) {
-    console.log('hover over here!');
-  });
+  
+  append_menu();
 });
 
 function add_base_link_styles(el) {
   $(el).addClass('add-icon');
+}
+
+function append_drop(el) {
+  return "<p>" + $(el).text() + "</p>";
+  // content = $(el).text()
+ 
+}
+
+function append_menu() {
+  console.log($('.field-menu').data('template'));
+   new Drop({
+     target: $('.field-menu')[0],
+     position: 'left middle',
+     content: $('.field-menu').data('template'),
+     openOn: 'hover',
+     classes: "the-menu"
+   })
 }
