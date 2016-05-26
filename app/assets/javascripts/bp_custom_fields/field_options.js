643cc07170bdb3d1a1798d0ddb378ef3fb01482e
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-05-26 19:36:24 -0400

    transfer

--- app/assets/javascripts/bp_custom_fields/field_options.js
+++ app/assets/javascripts/bp_custom_fields/field_options.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function() {
   
   $('.field_template-fields').on('cocoon:after-insert', function (e, insertedItem) {
     new_select = insertedItem.find('select.field-template-select-field-type').first();
-    previousItems = insertedItem.siblings().size() + 1
+    previousItems = insertedItem.siblings('.nested-fields').size() + 1
     insertedItem.find('.bpcf-field-header .bpcf-header-list-bottom li#order').text(previousItems)
     insertedItem.find('.bpcf-field-header .bpcf-header-list-bottom input[type="hidden"]').val(previousItems);
     bp_bind_type_select(new_select);
---
+++ app/assets/javascripts/bp_custom_fields/field_sorting.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+  var el = document.getElementById('field-template-sort-container');
+  var sortable = Sortable.create(el, {
+    handle: '.bpcf-header-list-top',
+    onEnd: function (e) {
+      console.log(e.oldIndex, e.newIndex);
+      $('.nested-fields').each(function(index) {
+        var new_index = index+1
+        $(this).find('.bpcf-field-header .bpcf-header-list-bottom li#order').text(new_index);
+        $(this).find('.bpcf-field-header .bpcf-header-list-bottom input[type="hidden"]').val(new_index);
+      });
+    }
+  });
+  
+  
+});
