75026458e4ecf24cfcb929de91a375b0362cec7d
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-06-17 13:50:29 -0400

    update action for bp_custom_fieldss

--- app/controllers/bp_custom_fields/abstract_resources_controller.rb
+++ app/controllers/bp_custom_fields/abstract_resources_controller.rb
@@ -21,6 +21,17 @@ module BpCustomFields
       end
     end
     
+    def update
+      @abstract_appearance = BpCustomFields::Appearance.abstract.where(resource_id: group_params[:abstract_appearance_resource_id]).first
+      @group = BpCustomFields::FieldManager.initialize_group_with_fields(@abstract_appearance.group_template)
+      @group.assign_attributes(group_params.except(:abstract_appearance_resource_id))
+      if @group.save
+        redirect_to bp_custom_fields.edit_abstract_resource_path(@abstract_appearance.resource_id)
+      else
+        render :edit
+      end
+    end
+    
     protected
     
     def group_params
