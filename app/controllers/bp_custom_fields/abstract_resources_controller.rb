c7ac23f2d642c84db2ccdbb41578b6b9674f31b7
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-06-17 14:01:08 -0400

    finish abstract resource creation

--- app/controllers/bp_custom_fields/abstract_resources_controller.rb
+++ app/controllers/bp_custom_fields/abstract_resources_controller.rb
@@ -10,23 +10,21 @@ module BpCustomFields
       @abstract_resource = BpCustomFields::AbstractResource.find(params[:id])
     end
     
-    def save
-      @abstract_appearance = BpCustomFields::Appearance.abstract.where(resource_id: group_params[:abstract_appearance_resource_id]).first
-      @group = BpCustomFields::FieldManager.initialize_group_with_fields(@abstract_appearance.group_template)
-      @group.assign_attributes(group_params.except(:abstract_appearance_resource_id))
-      if @group.save
-        redirect_to bp_custom_fields.edit_abstract_resource_path(@abstract_appearance.resource_id)
-      else
-        render :edit
-      end
-    end
+    # def save
+    #   @abstract_appearance = BpCustomFields::Appearance.abstract.where(resource_id: group_params[:abstract_appearance_resource_id]).first
+    #   @group = BpCustomFields::FieldManager.initialize_group_with_fields(@abstract_appearance.group_template)
+    #   @group.assign_attributes(group_params.except(:abstract_appearance_resource_id))
+    #   if @group.save
+    #     redirect_to bp_custom_fields.edit_abstract_resource_path(@abstract_appearance.resource_id)
+    #   else
+    #     render :edit
+    #   end
+    # end
     
     def update
-      @abstract_appearance = BpCustomFields::Appearance.abstract.where(resource_id: group_params[:abstract_appearance_resource_id]).first
-      @group = BpCustomFields::FieldManager.initialize_group_with_fields(@abstract_appearance.group_template)
-      @group.assign_attributes(group_params.except(:abstract_appearance_resource_id))
-      if @group.save
-        redirect_to bp_custom_fields.edit_abstract_resource_path(@abstract_appearance.resource_id)
+      @abstract_resource = BpCustomFields::AbstractResource.find(params[:id])
+      if @abstract_resource.update_attributes(abstract_resource_params)
+        redirect_to bp_custom_fields.edit_abstract_resource_path(@abstract_resource), notice: "Abstract Resource Updated!"
       else
         render :edit
       end
@@ -34,8 +32,8 @@ module BpCustomFields
     
     protected
     
-    def group_params
-      params.require(:group).permit(bpcf_fieldable_permitted_group_params(params))
+    def abstract_resource_params
+      params.require(:abstract_resource).permit(:id, bpcf_fieldable_permitted_params(params))
     end
   end
 end
